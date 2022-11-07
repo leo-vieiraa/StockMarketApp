ca0e85df2e2762dd02edf107058c0993e52b092c
leo-vieiraa <leovieira.devv@gmail.com>
2022-11-06 22:16:48 -0300

    feat[CAN-0006]: implemented companies listing screen and dependency injection

--- app/src/main/java/com/example/stockmarketapp/home/data/local/StockDao.kt
+++ app/src/main/java/com/example/stockmarketapp/home/data/local/StockDao.kt
@@ -1,9 +1,11 @@
 package com.example.stockmarketapp.home.data.local
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+@Dao
 interface StockDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
